70c4d6e0ad74bca52325d49efdad0479ab473e3d
AniruddhaGawali <aniruddhagawali05@gmail.com>
2023-01-06 18:15:27 +0530

    added head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Password Manager</title>
+        <meta
+          name="description"
+          content="Keep your passwords safe and organized with our password manager frontend app. Sign up now and enjoy convenient, secure password management."
+        />
+        <meta
+          name="keywords"
+          content="password manager, frontend, security, web app, passwords"
+        />
+        <meta name="author" content="RunTime-Terror" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="canonical"
+          href="https://password-manager-frontend.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://password-manager-frontend.vercel.app/"
+        />
+        <meta property="og:title" content="Password Manager Frontend" />
+        <meta
+          property="og:description"
+          content="Keep your passwords safe and organized with our password manager frontend app. Sign up now and enjoy convenient, secure password management."
+        />
+        <meta
+          property="og:image"
+          content="https://password-manager-frontend.vercel.app/images/logo.png"
+        />
+        <link rel="icon" href="/blackhole.gif" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
