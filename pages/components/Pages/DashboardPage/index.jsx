1d1b144f8c1991a8fca794c18c94f43659d1dd8e
Mohit Raut <rautmohit063@gmail.com>
2023-01-20 13:57:41 +0530

    added

--- pages/components/Pages/DashboardPage/index.jsx
+++ pages/components/Pages/DashboardPage/index.jsx
@@ -4,11 +4,13 @@ import Accounts_list from "./AccountsList";
 // import style module
 import styles from "./style.module.css";
 
+
 const DashboardPage = (props) => {
   return (
     <>
       <div className={styles.Dashboard}>{props.userID}</div>
       <Accounts_list/>
+      
     </>
   );
 };
