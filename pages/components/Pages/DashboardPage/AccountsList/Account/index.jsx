58ee4be54011efe93079194f356e584aceb6091e
Freakyab <aryanbhisikar1@gmail.com>
2023-01-20 22:12:55 +0530

    code verified

--- pages/components/Pages/DashboardPage/AccountsList/Account/index.jsx
+++ pages/components/Pages/DashboardPage/AccountsList/Account/index.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import style from "./styles.module.css";
+import style from "./style.module.css";
+import {GiPowerButton} from "react-icons/gi"
 // import style from "../../../DashboardPage/AccountsList/Account/style.module.css"
 // import "./style.module.css";
 
 function Account() {
   return (
     <>
-    <div className='Account'>
-      <div className='field'>
+    <div className={style.Account}>
+    {/* <div className="Account"> */}
+      <div className={style.field}>
         <label>UserName</label>
         <input type="text" name="username" placeholder='Username'/>
       </div>
-      <div className='field'>
+      <div className={style.field}>
         <label>Email</label>
         <input type="email" name="email" placeholder="Email"/>
       </div>
-      <div className='field'>
+      <div className={style.field}>
         <label>Password</label>
         <input type="password" name="password" placeholder="Password"/>
       </div>
-      <button className='field ui button'>Save</button>
+      <button className={style.fielduibutton}>
+        <GiPowerButton/>
+        Save</button>
       </div>
 
     </>
