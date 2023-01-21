b668f90296ddc29a2a23c3a0f954f51466eadf4c
AniruddhaGawali <aniruddhagawali05@gmail.com>
2023-01-21 23:11:10 +0530

    remove navbar error

--- pages/components/Navbar/index.jsx
+++ pages/components/Navbar/index.jsx
@@ -17,79 +17,88 @@ import { BsFillShieldLockFill } from "react-icons/bs";
 const Navbar = (props) => {
   return (
     <>
-      <MobileNavbar />
-      <nav className={style.Navbar_Wrapper}>
-        <div className={style.Logo}>
-          <BsFillShieldLockFill className={style.Logo_icon} />
-          <span>Password Manager</span>
-        </div>
-
-        <ul className={style.Navbar_Menu}>
-          <li className={style.active}>
-            <IoMdLock size="1.5rem" className={style.Navbar_Menu_icon} />{" "}
-            <span>all passwords</span>
-          </li>
-          <li>
-            <RxCounterClockwiseClock
-              size="1.5rem"
-              className={style.Navbar_Menu_icon}
-            />{" "}
-            <span>recents</span>
-          </li>
-          <li>
-            <BsStarFill size="1.3rem" className={style.Navbar_Menu_icon} />{" "}
-            <span>favorites</span>
-          </li>
-          <li>
-            <FaLayerGroup />
-            <span>groups</span>
-          </li>
-
-          <li>
-            <FaLayerGroup className={style.Navbar_Menu_icon} />
-            <span>groups</span>
-          </li>
-          <li>
-            <FaLayerGroup className={style.Navbar_Menu_icon} />
-            <span>groups</span>
-          </li>
-
-          <li>
-            <FaArchive className={style.Navbar_Menu_icon} />
-            <span>archive</span>
-          </li>
-          <li>
-            <AiFillDelete size="1.5rem" className={style.Navbar_Menu_icon} />
-            <span>trash</span>
-          </li>
-        </ul>
-
-        <div className={style.user}>
-          <div className={style.user_info}>
-            <span>
-              <FaUserAlt size={"1rem"} />
-            </span>{" "}
-            <div className={style.userName}>
-              {props.userData.UserInfo.userName}
+      {props.userData === null ? (
+        <></>
+      ) : (
+        <>
+          <MobileNavbar />
+          <nav className={style.Navbar_Wrapper}>
+            <div className={style.Logo}>
+              <BsFillShieldLockFill className={style.Logo_icon} />
+              <span>Password Manager</span>
             </div>
-          </div>
-          <div className={style.user_options}>
-            <IoIosSettings
-              size={"1.5rem"}
-              className={`${style.settings} ${style.user_options_icons}`}
-            />
-            <BiLogOut
-              size={"1.5rem"}
-              className={` ${style.user_options_icons}`}
-              onClick={() => {
-                Router.push({
-                  pathname: "/",
-                });
-              }}
-            />
-          </div>
-        </div>
-      </nav>
+
+            <ul className={style.Navbar_Menu}>
+              <li className={style.active}>
+                <IoMdLock size="1.5rem" className={style.Navbar_Menu_icon} />{" "}
+                <span>all passwords</span>
+              </li>
+              <li>
+                <RxCounterClockwiseClock
+                  size="1.5rem"
+                  className={style.Navbar_Menu_icon}
+                />{" "}
+                <span>recents</span>
+              </li>
+              <li>
+                <BsStarFill size="1.3rem" className={style.Navbar_Menu_icon} />{" "}
+                <span>favorites</span>
+              </li>
+              <li>
+                <FaLayerGroup />
+                <span>groups</span>
+              </li>
+
+              <li>
+                <FaLayerGroup className={style.Navbar_Menu_icon} />
+                <span>groups</span>
+              </li>
+              <li>
+                <FaLayerGroup className={style.Navbar_Menu_icon} />
+                <span>groups</span>
+              </li>
+
+              <li>
+                <FaArchive className={style.Navbar_Menu_icon} />
+                <span>archive</span>
+              </li>
+              <li>
+                <AiFillDelete
+                  size="1.5rem"
+                  className={style.Navbar_Menu_icon}
+                />
+                <span>trash</span>
+              </li>
+            </ul>
+
+            <div className={style.user}>
+              <div className={style.user_info}>
+                <span>
+                  <FaUserAlt size={"1rem"} />
+                </span>{" "}
+                <div className={style.userName}>
+                  {props.userData.UserInfo.userName}
+                </div>
+              </div>
+              <div className={style.user_options}>
+                <IoIosSettings
+                  size={"1.5rem"}
+                  className={`${style.settings} ${style.user_options_icons}`}
+                />
+                <BiLogOut
+                  size={"1.5rem"}
+                  className={` ${style.user_options_icons}`}
+                  onClick={() => {
+                    Router.push({
+                      pathname: "/",
+                    });
+                  }}
+                />
+              </div>
+            </div>
+          </nav>
+        </>
+      )}
     </>
   );
 };
