bccce699ca8ad4e4b6978874f5443659f561e4e7
Mohit Raut <rautmohit063@gmail.com>
2023-01-21 20:04:40 +0530

    AccountsList Last draft.

--- pages/components/Pages/DashboardPage/AccountsList/index.jsx
+++ pages/components/Pages/DashboardPage/AccountsList/index.jsx
@@ -4,40 +4,34 @@ import style from "./style.module.css";
 import { BsStarFill } from "react-icons/bs";
 import { MdArrowForwardIos } from "react-icons/md";
 
-const Element = [
-  "Twitter",
-  "Facebookjksdhrkjwgfjsdbfs",
-  "Instagram",
-  "Netflix",
-  "Prime",
-];
+// Account list array
+const Element = ["Twitter", "Facebook", "Instagram", "Netflix", "Prime"];
 const Accounts_list = () => {
   return (
     <>
+      {/* App list container */}
       <div className={style.app_list}>
+        {/* Map Function to create accounts */}
         {Element.map((e, key) => {
           return (
             <>
+              {/* AccountsList container */}
               <div key={key} className={style.Accounts}>
+                {/* Accounts Logo container */}
                 <div className={style.favLogo}>
                   <BsStarFill />
                 </div>
-                <span
-                  className={style.AccountItem}
-                  style={{
-                    textOverflow: "ellipsis",
-                    overflow: "hidden",
-                    width: "50%",
-                  }}
-                >
-                  {e}
-                </span>
-                <div className={style.arrow}>
-                  <div className={style.moreIn}>
-                    <span className={style.AccountItem}>{key}</span>
 
-                    <MdArrowForwardIos size={"1.3rem"} />
-                  </div>
+                {/* Account Name */}
+                <span className={style.AccountItem}>{e}</span>
+
+                {/* container to show MoreInfo & Arrow */}
+                <div className={style.arrowInfo}>
+                  {/* Display Number of users */}
+                  <span className={style.noofItem}>{key}</span>
+
+                  {/* Arrow icon */}
+                  <MdArrowForwardIos size={"1.3rem"} />
                 </div>
               </div>
             </>
