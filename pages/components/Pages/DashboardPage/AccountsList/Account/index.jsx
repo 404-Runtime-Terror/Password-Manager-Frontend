185a6a387b772438188efd2298b9f216fa779127
Freakyab <aryanbhisikar1@gmail.com>
2023-01-22 14:29:21 +0530

    fetch account

--- pages/components/Pages/DashboardPage/AccountsList/Account/index.jsx
+++ pages/components/Pages/DashboardPage/AccountsList/Account/index.jsx
@@ -1,41 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./style.module.css";
-import {CiSaveDown2} from "react-icons/ci"
-// import style from "../../../DashboardPage/AccountsList/Account/style.module.css"
-// import "./style.module.css";
+import { CiSaveDown2 } from "react-icons/ci"
 
-function Account() {
+function Account(props) {
+  const { accounts } = props;
+  const [fetchaccount, setFetchaccount] = React.useState([]);
+  useEffect(() => {
+    if(props.index !== undefined && props.index !== null){
+      setFetchaccount(accounts[props.index]);
+    }
+    else{
+      setFetchaccount(accounts[0]);
+    }
+  }, [props.index]);
   return (
     <>
-    <div className={style.Account}>
-    {/* <div className="Account"> */}
+    {console.log(fetchaccount)}
+    {fetchaccount && fetchaccount.map((e,index) => {
+      return(
+        <div className={style.Account}>
+        <div className={style.field}>
+          <label>username</label>
+          <label>{fetchaccount[index].username}</label>
+          <input type="text" name="username" placeholder='Username' />
+        </div>
 
-    //UserName
-      <div className={style.field}>
-        <label>UserName</label>
-        <input type="text" name="username" placeholder='Username'/>
-      </div>
+        <div className={style.field}>
+          <label>{fetchaccount[index].password}</label>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
 
-      //Email
-      <div className={style.field}>
-        <label>Email</label>
-        <input type="email" name="email" placeholder="Email"/>
+        <button className={style.fielduibutton}>
+          <CiSaveDown2 />
+          Save</button>
       </div>
-
-      //Password
-      <div className={style.field}>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password"/>
-      </div>
-      
-      //Save
-      <button className={style.fielduibutton}>
-        <CiSaveDown2/>
-        Save</button>
-      </div>
+      )
+    }
+    )}
 
     </>
-    )
+  )
 }
 
 export default Account;
--- pages/components/Pages/DashboardPage/AccountsList/index.jsx
+++ pages/components/Pages/DashboardPage/AccountsList/index.jsx
@@ -1,51 +1,52 @@
 import React from "react";
 import style from "./style.module.css";
+import Link from "next/link";
 // import icons
 import { BsStarFill } from "react-icons/bs";
 import { MdArrowForwardIos } from "react-icons/md";
+import react from "react";
 
-const Element = [
-  "Twitter",
-  "Facebookjksdhrkjwgfjsdbfs",
-  "Instagram",
-  "Netflix",
-  "Prime",
-];
-const Accounts_list = () => {
+
+const Accounts_list = (props) => {
+  const { website } = props;
   return (
     <>
       <div className={style.app_list}>
-        {Element.map((e, key) => {
-          return (
-            <>
-              <div key={key} className={style.Accounts}>
-                <div className={style.favLogo}>
-                  <BsStarFill />
-                </div>
-                <span
-                  className={style.AccountItem}
-                  style={{
-                    textOverflow: "ellipsis",
-                    overflow: "hidden",
-                    width: "50%",
-                  }}
-                >
-                  {e}
-                </span>
-                <div className={style.arrow}>
-                  <div className={style.moreIn}>
-                    <span className={style.AccountItem}>{key}</span>
-
-                    <MdArrowForwardIos size={"1.3rem"} />
-                  </div>
+        {website.map((e, key) => (
+          // <Link href='/page/components/Pages/DashboardPage/AccountsList/Account' as={`/page/components/Pages/DashboardPage/AccountsList/Account/${key}`} key={key}>
+            <div className={style.Accounts}
+            onClick = {()=>{console.log(key)
+            props.setindex(key)}}
+                        >
+              {/* {props.setindex(key)} */}
+              <div className={style.favLogo}>
+                <BsStarFill />
+              </div>
+              <span
+                className={style.AccountItem}
+                style={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  width: "50%",
+                }}
+              >
+                {e}
+              </span>
+              <div className={style.arrow}>
+                <div className={style.moreIn}>
+                  <span className={style.AccountItem}>{key}</span>
+                  <MdArrowForwardIos size={"1.3rem"} />
                 </div>
               </div>
-            </>
-          );
-        })}
+            </div>
+          // </Link>
+        ))}
       </div>
     </>
   );
 };
 
 export default Accounts_list;
+
+  
+
